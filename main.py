d93e1be753f3c412a4b372d95d0068e5101252d0
ty444 <tomyoshi1995@gmail.com>
2020-05-25 23:29:44 +0900

    add templates

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
 
 
-class FirstQuestions:
-    bar1 = "\n==================================================\n Hi, What's your name ?? \n=================================================="
-    bar2 = "\n==================================================\n"
+# yet
 
-    def __init__(self):
-        # user input name
-        print(self.bar1)
-        user_name = input("→ :")
-        self.question1(user_name)
-
-    def question1(self, name):
-        print(self.bar2)
-        print("{}".format(name) + "!! " + "Where restaurant do you like??")
-        print(self.bar2)
-
-
-
-
-o = FirstQuestions()
